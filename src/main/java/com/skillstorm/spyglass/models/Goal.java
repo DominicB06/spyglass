cb6b3e53bb710293344cda5c0abf30c31084cc5b
DominicB06 <72121721+DominicB06@users.noreply.github.com>
2022-08-29 11:02:52 -0500

    Initial commit

---
+++ src/main/java/com/skillstorm/spyglass/models/Goal.java
@@ -0,0 +1,110 @@
+package com.skillstorm.spyglass.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "goals")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Goal {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "goal_id")
+	private int goalId;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "description")
+	private String desription;
+	
+	@Column(name = "picture")
+	private String picture;
+	
+	@Column(name = "target_date")
+	private LocalDate targetDate;
+	
+	@Column(name = "target_amount")
+	private double targetAmount;
+	
+	@Column(name = "current_amount")
+	private double currentAmount;
+	
+	@ManyToOne
+	@JoinColumn(name = "user")
+	private User user;
+	
+	
+	public Goal() {
+		super();
+	}
+	
+	public int getGoalId() {
+		return goalId;
+	}
+	public void setGoalId(int goalId) {
+		this.goalId = goalId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesription() {
+		return desription;
+	}
+	public void setDesription(String desription) {
+		this.desription = desription;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public LocalDate getTargetDate() {
+		return targetDate;
+	}
+	public void setTargetDate(LocalDate targetDate) {
+		this.targetDate = targetDate;
+	}
+	public double getTargetAmount() {
+		return targetAmount;
+	}
+	public void setTargetAmount(double targetAmount) {
+		this.targetAmount = targetAmount;
+	}
+	public double getCurrentAmount() {
+		return currentAmount;
+	}
+	public void setCurrentAmount(double currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@Override
+	public String toString() {
+		return "goal [goalId=" + goalId + ", name=" + name + ", desription=" + desription + ", picture=" + picture
+				+ ", targetDate=" + targetDate + ", targetAmount=" + targetAmount + ", currentAmount=" + currentAmount
+				+ ", user=" + user + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/skillstorm/spyglass/models/User.java
@@ -0,0 +1,94 @@
+package com.skillstorm.spyglass.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "users")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private int userId;
+	
+	@Column(name = "fname")
+	private String fname;
+	
+	@Column(name = "lname")
+	private String lname;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@OneToMany(mappedBy = "User")
+	@JsonIgnore
+	private List<Goal> goals;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", password="
+				+ password + "]";
+	}
+	
+	
+}
