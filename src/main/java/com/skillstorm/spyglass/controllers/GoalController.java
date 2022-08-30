21ab8fa53d2051803f6de7a56bfc6aebc1a807f1
DominicB06 <72121721+DominicB06@users.noreply.github.com>
2022-08-30 11:43:41 -0500

    Added swagger docs

--- src/main/java/com/skillstorm/spyglass/controllers/GoalController.java
+++ src/main/java/com/skillstorm/spyglass/controllers/GoalController.java
@@ -18,32 +18,47 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skillstorm.spyglass.models.Goal;
 import com.skillstorm.spyglass.services.GoalService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/goals")
+@Tag(name = "Goals")
+@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Successful"),
+		@ApiResponse(responseCode = "201", description = "Created"),
+		@ApiResponse(responseCode = "401", description = "Unauthorized")
+})
 public class GoalController {
 
 	@Autowired
 	private GoalService service;
 	
 	@GetMapping("/user/{userId}")
+	@Operation(summary = "Find all goals for a user", description = "returns a list of goals")
 	public ResponseEntity<List<Goal>> findByUser(@PathVariable(value = "userId") int userId) {
 		List<Goal> goals = service.findByUser(userId);
 		return ResponseEntity.ok(goals);
 	}
 	
 	@PostMapping
+	@Operation(summary = "Create a new goal", description = "returns the created goal")
 	public ResponseEntity<Goal> createGoal(@Valid @RequestBody Goal goal){
 		Goal g = service.createGoal(goal);
 		return ResponseEntity.ok(g);
 	}
 	
 	@PutMapping
+	@Operation(summary = "Update an existing goal", description = "returns the updated goal")
 	public ResponseEntity<Goal> updateGoal(@Valid @RequestBody Goal goal){
 		Goal g = service.updateGoal(goal);
 		return ResponseEntity.ok(g);
 	}
 	
 	@DeleteMapping("/{goalId}")
+	@Operation(summary = "Delete a goal", description = "Returns true or false depending on succes")
 	public ResponseEntity<Boolean> deleteGoal(@PathVariable(value = "goalId") int goalId){
 		service.deleteGoal(goalId);
 		return ResponseEntity.ok(true);
--- src/main/java/com/skillstorm/spyglass/controllers/UserController.java
+++ src/main/java/com/skillstorm/spyglass/controllers/UserController.java
@@ -18,32 +18,54 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skillstorm.spyglass.models.User;
 import com.skillstorm.spyglass.services.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/users")
+@Tag(name ="Users")
+@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Successful"),
+		@ApiResponse(responseCode = "201", description = "Created"),
+		@ApiResponse(responseCode = "401", description = "Unauthorized")
+})
 public class UserController {
 
 	@Autowired
 	private UserService service;
 	
 	@GetMapping("/user/{userId}")
+	@Operation(summary = "Find user by id", description = "returns the user")
 	public ResponseEntity<Optional<User>> findById(@PathVariable(value = "userId") int userId) {
 		Optional<User> user = service.findById(userId);
 		return ResponseEntity.ok(user);
 	}
 	
+	@GetMapping("/user/email/{email}")
+	@Operation(summary = "Find user by id", description = "returns the user")
+	public ResponseEntity<User> findByEmail(@PathVariable(value = "email") String email) {
+		User user = service.findByEmail(email);
+		return ResponseEntity.ok(user);
+	}
+	
 	@PostMapping
+	@Operation(summary = "Create a new user", description = "returns the created user")
 	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
 		User u = service.createUser(user);
 		return ResponseEntity.ok(u);
 	}
 	
 	@PutMapping
+	@Operation(summary = "Update an existing user", description = "returns the updated user")
 	public ResponseEntity<User> updateUser(@Valid @RequestBody User user){
 		User u = service.updateUser(user);
 		return ResponseEntity.ok(u);
 	}
 	
 	@DeleteMapping("/{userId}")
+	@Operation(summary = "Deletes an user", description = "returns true or false depending on success")
 	public ResponseEntity<Boolean> deleteUser(@PathVariable(value = "userId") int userId){
 		service.deleteUser(userId);
 		return ResponseEntity.ok(true);
